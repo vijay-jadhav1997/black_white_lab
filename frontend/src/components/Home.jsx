2216db9716c155c8c4f4e6478e33bc0732bbca2f
vijay-jadhav1997 <vijay05111997@gmail.com>
2025-01-18 18:44:18 +0530

    feat: add image uploading feature

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react"
+import { useState } from "react"
 // import {childBnWImg} from '../assets/img/child-black-white.jpg'
 import childBnWImg from '../assets/img/child-black-white.jpg'
 // import {oceanBnWImg} from '../assets/img/color-black-white-9.jpg'
@@ -6,57 +6,99 @@ import childBnWImg from '../assets/img/child-black-white.jpg'
 
 function Home() {
   const [message, setMessage] = useState('')
+  const [imageFile, setImageFile] = useState(false)
 
-  useEffect(() => {
-    (async function() {
-      const response = await fetch('http://127.0.0.1:8000/index/')
-      const result =  await response.json()
-      console.log(result);
-      if(message === 'Jay Hari') {
-        setMessage(result.message)
-      }
-    })()
-  }, [message])
+
+  function handleChange(e) {
+    e.preventDefault()
+    // console.dir(e.target);
+    
+    setImageFile(e.target.files[0])
+  }
+
+    //   console.log(imageFile);
+  
+
+  async function handleSubmit(e) {
+    e.preventDefault()
+
+    if (!imageFile) {
+      setMessage("Please select an image to upload.")
+      return
+    }
+
+    const formData = new FormData()
+    formData.append('img', imageFile)
+
+    try {
+        const response = await fetch('', {
+            method: 'POST',
+            body: {...formData}
+        })
+
+        if (response.ok) {
+            const responseData = await response.json()
+            setMessage(responseData?.message || 'Image uploaded successfully!')
+        } else {
+            const error = await response.json()
+            setMessage(`Error: ${JSON.stringify(error)}`)
+        }
+    } catch (error) {
+        console.error('Error: ', error)
+        setMessage("Something went wrong. Please, try again!")
+    }
+
+  }
   
   return (
-    <div className="bg-gradient-to-b from-green-50 to-green-100">
+    <main className="bg-gradient-to-b from-green-50 to-green-100">
+        <section className="py-12 sm:py-8 lg:py-20">
+            <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
+                <div className="grid items-center grid-cols-1 gap-12 lg:grid-cols-2">
+                    <div>
+                        <h1 className="text-4xl font-bold text-black sm:text-6xl lg:text-7xl">
+                            Transform your Images, with
+                            <div className="relative inline-flex">
+                                {/* <span className="absolute inset-x-0 bottom-0 border-b-[30px] border-[#4ADE80]"></span> */}
+                                <span className="heading relative text-4xl font-bold text-white bg-gray-800 sm:text-6xl lg:text-7xl">BlackWhiteLab.</span>
+                            </div>
+                        </h1>
 
-    <section className="py-10 sm:py-16 lg:py-24">
-        <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
-            <div className="grid items-center grid-cols-1 gap-12 lg:grid-cols-2">
-                <div>
-                    <h1 className="text-4xl font-bold text-black sm:text-6xl lg:text-7xl">
-                        Transform your Images, with
-                        <div className="relative inline-flex">
-                            {/* <span className="absolute inset-x-0 bottom-0 border-b-[30px] border-[#4ADE80]"></span> */}
-                            <h1 className="heading relative text-4xl font-bold text-black sm:text-6xl lg:text-7xl">BlackWhiteLab.</h1>
+                        <p className="mt-8 text-base text-black sm:text-xl">Discover the beauty of simplicity with our cutting-edge image and video processing application. Convert your photos and videos into stunning black-and-white masterpieces effortlessly.</p>
+
+                        <div className="mt-10 sm:flex sm:items-center sm:space-x-8">
+                            <a href="#img-processing-container" title="" className="inline-flex items-center justify-center px-10 py-4 text-base font-semibold text-white transition-all duration-200 bg-orange-500 hover:bg-orange-600 focus:bg-orange-600" role="button"> Start exploring </a>
                         </div>
-                    </h1>
+                    </div>
 
-                    <p className="mt-8 text-base text-black sm:text-xl">Discover the beauty of simplicity with our cutting-edge image and video processing application. Convert your photos and videos into stunning black-and-white masterpieces effortlessly.</p>
-
-                    <div className="mt-10 sm:flex sm:items-center sm:space-x-8">
-                        <a href="#" title="" className="inline-flex items-center justify-center px-10 py-4 text-base font-semibold text-white transition-all duration-200 bg-orange-500 hover:bg-orange-600 focus:bg-orange-600" role="button"> Start exploring </a>
-
-                        <a href="#" title="" className="inline-flex items-center mt-6 text-base font-semibold transition-all duration-200 sm:mt-0 hover:opacity-80">
-                            <svg className="w-10 h-10 mr-3" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path fill="#F97316" stroke="#F97316" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M14.752 11.168l-3.197-2.132A1 1 0 0010 9.87v4.263a1 1 0 001.555.832l3.197-2.132a1 1 0 000-1.664z" />
-                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-                            </svg>
-                            Watch video
-                        </a>
+                    <div>
+                        <img className="w-full" src={childBnWImg} alt="" />
                     </div>
                 </div>
-
-
-                <div>
-                    <img className="w-full" src={childBnWImg} alt="" />
-                    {/* <img className="w-full" src="https://cdn.rareblocks.xyz/collection/celebration/images/hero/2/hero-img.png" alt="" /> */}
-                </div>
             </div>
-        </div>
-    </section>
-</div>
+        </section>
+
+        <section className="mt-2 py-5 sm:py-16 lg:py-24">
+            <h3 className="mb-4 py-2 my-2 font-semibold text-3xl">Try Your Own Image</h3>
+            {message === '' ? '' : <p className="my-4 text-base text-red-600">{message}</p>}
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="file"
+                    name="img"
+                    id="image"
+                    accept="image/*"
+                    onChange={(e) => handleChange(e)}
+                    placeholder="Select Your Image"
+                    className="block w-[80%] mx-auto py-4 pl-10 pr-4 border-blue-600 cursor-pointer text-black placeholder-gray-500 transition-all duration-200 border-dashed border-2 rounded-md bg-gray-50 focus:outline-none focus:border-blue-600 focus:bg-white caret-blue-600"
+                    required
+                />
+                {/* <label htmlFor="image" className="font-semibold text-2xl text-gray-900"> Select your Image </label> */}
+                <button type="submit"
+                    className="inline-flex items-center justify-center w-max mt-6 px-10 py-2 text-base font-semibold text-white transition-all duration-200 bg-amber-600 border border-transparent rounded-md focus:outline-none hover:bg-amber-500 focus:bg-amber-700"
+                >Upload</button>
+            </form>
+        </section>
+    </main>
 
   )
 }
